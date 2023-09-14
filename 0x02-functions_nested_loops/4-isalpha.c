aef3b31bf4dc985f6b9128607d5468b9d190c839
ItsKendi <itswendykendi@gmail.com>
2023-09-14 12:22:56 +0300

    updating the is alpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
+ * _isalpha - function name
+ * @c : varable
+ * description : checks if it is alpha
+ *
+ * Return: int
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
