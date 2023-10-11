27f3177964c77286a1b60be4039e229e946abcd0
ItsKendi <itswendykendi@gmail.com>
2023-10-11 10:56:14 +0300

    Main header prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int m);
 int is_prime_number(int n);int is_palindrome(char *s);
+int is_prime(int n, int start);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
 #endif
