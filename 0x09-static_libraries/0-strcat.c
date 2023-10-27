3282c03bd52811ff0807d84f432202cecea4063c
ItsKendi <itswendykendi@gmail.com>
2023-10-28 01:33:30 +0300

    Static libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,21 +1,16 @@
 #include "main.h"
+#include <string.h>
+
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will append
- *@src: string that will be concatenated upon
+ * _strncat - prints a concatenated string
+ * @dest: parameter to store concatenated string
+ * @src: source of the string
+ * @n: n bytes from src
  *
- * Return: returns pointer to @dest
+ * Return: dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	return (strncat(dest, src, n));
+}
--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,28 +1,15 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strspn - getting the length of a prefix substring
- * @s: the string
+ * @s: the strin
  * @accept: substring
  *
  * Return: length
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
-	int isAccepted[256] = {0};
-
-	while (*accept)
-	{
-		isAccepted[(unsigned char)*accept] = 1;
-		accept++;
-	}
-
-	while (*s && isAccepted[(unsigned char)*s])
-	{
-		count++;
-		s++;
-	}
-
-	return (count);
+	return (strspn(s, accept));
 }
