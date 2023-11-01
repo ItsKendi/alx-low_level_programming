587d8b2366deba74bbedef05c640fa020c400caf
ItsKendi <itswendykendi@gmail.com>
2023-11-01 21:53:15 +0300

    Pushing all changes - incomplete commits

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,39 +3,39 @@
 #include <string.h>
 
 /**
- * alloc_grid - return a two dimentional array
- * @width: the width
- * @height: the height
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
  *
- * Return: a two dimentional array
+ * Return: concatenated strings; NULL on failure
  */
-
-int **alloc_grid(int width, int height)
+char *str_concat(char *s1, char *s2)
 {
-	int **arr;
-	int i;
+	char *new_string;
+	int i, len_s1, len_s2;
 
-	if (width == 0 || height == 0)
-	{
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	new_string = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+
+	if (new_string == NULL)
 		return (NULL);
-	}
 
-	arr = (int **)malloc(height * sizeof(int *));
+	for (i = 0; i < len_s1; i++)
+		new_string[i] = s1[i];
 
-	if (arr == 0)
-	{
-		return (NULL);
-	}
+	for (i = 0; i < len_s2; i++)
+		new_string[len_s1 + i] = s2[i];
 
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = malloc(width * sizeof(int));
-	}
+	new_string[len_s1 + len_s2] = '\0';
 
-	if (arr != NULL)
-	{
-		return (arr);
-	}
-
-	return (NULL);
+	return (new_string);
 }
+
